2dbe617fb7fea634420e34634b8e611d762aad6b
san0suke <sistemas.robson@gmail.com>
2022-01-01 11:54:55 -0300

    Player Input Start

---
+++ ProjetoComunitario/UnityComunitario/Assets/Scenes/CenaInicial/Scripts/CenaInicialController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CenaInicialController : MonoBehaviour
+{
+    internal PlayerControls playerControls;
+
+    private void Start()
+    {
+        playerControls.PlayerMenuControls.Start.performed += context => StartPressed(context);
+    }
+
+    private void StartPressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("StartPressed");
+    }
+
+    private void OnEnable()
+    {
+        playerControls = new PlayerControls();
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
